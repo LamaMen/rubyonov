2395ea48248594784b73ed2f896e13e9d4b022ea
Ilia Rodionov <i.rodionov@allerhand.ru>
2024-05-10 20:50:36 +0300

    Implement simple results views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
     include SessionsHelper
+
+    def pages_of(data, records_per_pages)
+        unless data.kind_of?(Array)
+          data.page(params[:page]).per(records_per_pages)
+        else
+          Kaminari.paginate_array(data).page(params[:page]).per(records_per_pages)
+        end
+    end
 end
--- app/controllers/work_controller.rb
+++ app/controllers/work_controller.rb
@@ -22,7 +22,7 @@ class WorkController < ApplicationController
     @image_data = {}
     # I18n.locale = session[:current_locale]
    
-    # current_user_id = current_user.id
+    current_user_id = current_user.id
     if params[:theme] == "-----" #.blank?
       theme = "Select theme to leave your answer"
       theme_id = 1
@@ -40,6 +40,15 @@ class WorkController < ApplicationController
     end
     session[:selected_theme_id] = theme_id
     image_data(theme, data)
-   end
+  end
    
+  def results_list
+    @selected_theme_id = session[:selected_theme_id]
+    
+    res_composite_diag = Image.where(theme_id: @selected_theme_id).order("ave_value DESC")
+    composite_results_size = res_composite_diag.size
+    
+    @composite_results = res_composite_diag.take(composite_results_size)
+    @composite_results_paged = pages_of(@composite_results, 2)
+  end
 end
